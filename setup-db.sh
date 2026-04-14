115506a0bac3c73ce084ff43870ae1b4258aa45f
swapnilpadwal311 <swapnilpadwal311@gmail.com>
2026-04-14 13:08:21 +0530

    Update setup-db.sh

--- setup-db.sh
+++ setup-db.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+set -e  # stop script immediately if any command fails
+
 DB_HOST="database-2.cnew8imesw19.ap-south-1.rds.amazonaws.com"
 DB_PORT="3306"
+
 DB_USER="admin"
-DB_PASS="admin123"
+DB_PASS="admin123@2026"
+
 DB_NAME="database-2"
 
 echo "Connecting to AWS RDS..."
+
 mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USER" -p"$DB_PASS" <<SQL
-CREATE DATABASE IF NOT EXISTS \`database-2\`;
-USE \`database-2\`;
+
+CREATE DATABASE IF NOT EXISTS \`$DB_NAME\`;
+
+USE \`$DB_NAME\`;
+
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(100),
@@ -19,10 +27,3 @@ CREATE TABLE IF NOT EXISTS users (
 SQL
 
 echo "Database setup completed successfully"
-
-EOF
-
-echo "Database setup completed successfully!"
-~
-~
-~
